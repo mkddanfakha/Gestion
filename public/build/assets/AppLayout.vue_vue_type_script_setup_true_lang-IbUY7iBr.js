6e6bced004bc76b3f6d2d833c53943f0dd77c6a8
dmoha <dmohamedkounta@gmail.com>
2025-12-05 18:08:05 +0100

    build prod

--- public/build/assets/AppLayout.vue_vue_type_script_setup_true_lang-IbUY7iBr.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./BootstrapLayout-DOjYiQwv.js";import{d as t,c as o,w as s,I as c,o as u}from"./app-CKyF0dn4.js";const n=t({__name:"AppLayout",props:{breadcrumbs:{default:()=>[]}},setup(r){return(a,m)=>(u(),o(e,{breadcrumbs:r.breadcrumbs},{default:s(()=>[c(a.$slots,"default")]),_:3},8,["breadcrumbs"]))}});export{n as _};
